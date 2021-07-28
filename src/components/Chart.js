d4133793211b4b41cb326ca2fdf1cb98dfba9999
suvel-the-dev <84700466+suvel-the-dev@users.noreply.github.com>
2021-07-28 12:23:52 +0530

    Added Pie Chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,30 +1,15 @@
 import React from "react";
 import { Bar, Line, Pie } from "react-chartjs-2";
-import { default as pieChart } from "../pie_chart";
+import { default as barChart } from "../bar_chart";
 import { default as lineChart } from "../line_chart";
+import { default as pieChart } from "../pie_chart";
 
 function Chart(props) {
   return (
     <div className="chart">
-      <Bar data={pieChart.data} options={pieChart.options} />
+      <Bar data={barChart.data} options={barChart.options} />
       <Line data={lineChart.data} options={lineChart.options} />
-      {/* <Pie
-        data={props.chartData}
-        options={{
-          title: {
-            display: props.displayTitle,
-            text: 'Largest Cities in Massachusetts',
-            fontSize: 25
-          },
-          legend: {
-            display: props.displayLegend,
-            position: props.legendPosition,
-            labels: {
-              fontColor: '#000'
-            }
-          }
-        }}
-      /> */}
+      <Pie data={pieChart.data} options={pieChart.options} />
     </div>
   );
 }
--- src/pie_chart.js
+++ src/pie_chart.js
@@ -1,55 +1,22 @@
 const chartData = {
   data: {
-    labels: [
-      "CP250A",
-      "CP250B",
-      "CP250C",
-      "CP250D",
-      "CP251A",
-      "CP251B",
-      "CP251C",
-      "RR241A",
-      "RR241B",
-      "RR241C",
-      "RR241D"
-    ],
+    labels: ["CP", "RR"],
     datasets: [
       {
-        data: [59.55, 50, 60, 50, 80, 100, 54.54, 91.18, 100, 100, 96],
-        backgroundColor: [
-          "#3794ff",
-          "#3794ff",
-          "#3794ff",
-          "#3794ff",
-          "#3794ff",
-          "#3794ff",
-          "#3794ff",
-          "#ffff00",
-          "#ffff00",
-          "#ffff00",
-          "#ffff00"
-        ]
+        data: [65, 92],
+        backgroundColor: ["#3794ff", "#ffff00"]
       }
     ]
   },
   options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true
-          }
-        }
-      ]
-    },
     title: {
       display: true,
-      text: "Zone-ABCD Utilization",
+      text: "Zone Utilization",
       fontSize: 25
-    },
-    legend: {
-      display: false
     }
+    // legend: {
+    //   display: false
+    // }
   }
 };
 
